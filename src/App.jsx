c59040a52dcf23c2eee0ac936efb8a85d53a9c10
Aradhana Nayak <aradhana.zero@gmail.com>
2024-06-27 13:34:22 +0530

    added new styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,39 @@
-import { useState } from 'react'
-import './App.css'
-import { FileExplorer } from './components/file-explorer'
-import { Navbar } from './components/navbar'
-import { ThemeProvider } from './components/provider/theme-provider'
-import explorer from './data/folderData'
-import useTraverseTree from './hooks/use-traverse-tree'
-import { Toaster } from 'sonner'
+import { useState } from "react";
+import "./App.css";
+import { FileExplorer } from "./components/file-explorer";
+import { Navbar } from "./components/navbar";
+import { ThemeProvider } from "./components/provider/theme-provider";
+import explorer from "./data/folderData";
+import useTraverseTree from "./hooks/use-traverse-tree";
+import { Toaster } from "sonner";
 
 function App() {
-  const [explorerData, setExplorerData] = useState(explorer)
+  const [explorerData, setExplorerData] = useState(explorer);
 
   const { insertNode } = useTraverseTree();
   const handleInsertNode = (folderId, item, isFolder) => {
     const finalTree = insertNode(explorerData, folderId, item, isFolder);
 
     setExplorerData(finalTree);
-  }
+  };
 
   return (
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
       <Toaster richColors position="bottom-center" />
-      <div className="font-JetMono min-h-screen mx-auto">
+      <div className="font-JetMono min-h-screen mx-auto bg-[#f3f3f3] dark:bg-neutral-900">
         <Navbar />
 
-        <div className="p-5 max-w-2xl h-[70vh] border shadow-md rounded-md bg-gray-200 dark:bg-neutral-700 mx-auto mt-10">
+        <div className="p-5 max-w-2xl h-[70vh] border shadow-md rounded-md bg-zinc-300 dark:bg-neutral-700 mx-auto mt-10">
+          <h1 className="text-2xl font-bold py-2">Explorer</h1>
 
-          <h1 className="text-2xl font-bold py-2">
-            Explorer
-          </h1>
-
-          <FileExplorer handleInsertNode={handleInsertNode} explorer={explorerData} />
+          <FileExplorer
+            handleInsertNode={handleInsertNode}
+            explorer={explorerData}
+          />
         </div>
-
       </div>
     </ThemeProvider>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/file-explorer.jsx
+++ src/components/file-explorer.jsx
@@ -1,110 +1,114 @@
 import { cn } from "@/lib/utils";
-import { useState } from "react"
+import { useState } from "react";
 import { Button } from "./ui/button";
 import { File, FilePlus, Folder, FolderOpen, FolderPlus } from "lucide-react";
 import { Input } from "./ui/input";
 import { toast } from "sonner";
 
 export const FileExplorer = ({ handleInsertNode, explorer }) => {
+  if (explorer.isFolder) {
+    const [expand, setExpand] = useState(false);
+    const [showInput, setShowInput] = useState({
+      visible: false,
+      isFolder: null,
+    });
 
-    if (explorer.isFolder) {
-        const [expand, setExpand] = useState(false);
-        const [showInput, setShowInput] = useState({
-            visible: false,
-            isFolder: null
-        });
+    const handleNewFolder = (e, isFolder) => {
+      e.stopPropagation();
+      setExpand(true);
 
-        const handleNewFolder = (e, isFolder) => {
-            e.stopPropagation();
-            setExpand(true);
+      setShowInput({
+        visible: true,
+        isFolder,
+      });
+    };
 
-            setShowInput({
-                visible: true,
-                isFolder
-            })
-        }
+    const onAddFolder = (e) => {
+      if (e.keyCode === 13 && e.target.value) {
+        handleInsertNode(explorer.id, e.target.value, showInput.isFolder);
+        toast.success(
+          `Added ${showInput.isFolder ? "Folder" : "File"} ${e.target.value}`
+        );
+        setShowInput({ ...showInput, visible: false });
+      }
+    };
 
-        const onAddFolder = (e) => {
-            if (e.keyCode === 13 && e.target.value) {
-                handleInsertNode(explorer.id, e.target.value, showInput.isFolder);
-                toast.success(`Added ${showInput.isFolder ? "Folder" : "File"} ${e.target.value}`)
-                setShowInput({ ...showInput, visible: false })
-            }
-        }
+    return (
+      <div>
+        <div className="flex justify-between">
+          <Button
+            className="flex items-center cursor-pointer"
+            variant="ghost"
+            onClick={() => setExpand(!expand)}
+          >
+            {expand ? (
+              <FolderOpen
+                fill="#FFBF00"
+                className="w-5 h-5 mr-2 dark:text-black"
+              />
+            ) : (
+              <Folder className="w-5 h-5 text-amber-600 mr-2" fill="#FFBF00" />
+            )}{" "}
+            {explorer.name}
+          </Button>
 
-
-
-        return (
-            <div>
-                <div className="flex justify-between">
-
-                    {/* 📁 {explorer.name} */}
-                    <Button
-                        className="flex items-center cursor-pointer"
-                        variant="ghost"
-                        onClick={() => setExpand(!expand)}
-                    >
-                        {expand ? <FolderOpen
-                            fill="#FFBF00" className="w-5 h-5 mr-2 dark:text-black" /> : <Folder className="w-5 h-5 text-[#FFBF00] mr-2" />} {explorer.name}
-                    </Button>
-
-
-                    <div className="flex items-center gap-2">
-                        <Button
-                            size="sm"
-                            variant="outline"
-                            onClick={(e) => handleNewFolder(e, true)}
-                        >
-                            Folder
-                            <FolderPlus className="ml-2 w-5 h-5" />
-                        </Button>
-                        <Button
-                            onClick={(e) => handleNewFolder(e, false)}
-                            size="sm"
-                            variant="outline"
-                        >
-                            File
-                            <FilePlus className="ml-2 w-5 h-5" />
-                        </Button>
-                    </div>
-                </div>
-
-                <div className={cn("ml-6 flex flex-col", expand ? "block" : "hidden")}>
-
-                    {
-                        showInput.visible && (
-                            <div className="flex gap-2 items-center transition">
-                                <span>{showInput.isFolder ? <Folder className="w-5 h-5 text-[#FFBF00]" /> : <File className="w-5 h-5 text-blue-500" />}
-                                </span>
-
-                                <Input
-                                    autoFocus
-                                    type="text"
-                                    onBlur={() => setShowInput({ ...showInput, visible: false })}
-                                    className="w-[30vw] h-10"
-                                    onKeyDown={onAddFolder}
-                                />
-                            </div>
-                        )
-                    }
-                    {explorer.items.map((item) => {
-                        return (
-                            <FileExplorer handleInsertNode={handleInsertNode} explorer={item} key={item.id} />
-                        )
-                    })}
-                </div>
-            </div >
-        )
-    }
-    else {
-        return (
-            // <span className="flex">📄 {explorer.name}</span>
+          <div className="flex items-center gap-2">
             <Button
-                className="flex items-center cursor-pointer"
-                variant="ghost"
+              size="sm"
+              variant="outline"
+              onClick={(e) => handleNewFolder(e, true)}
             >
-                <File className="w-5 h-5 text-blue-500 mr-2" /> {explorer.name}
+              Folder
+              <FolderPlus className="ml-2 w-5 h-5 text-amber-600" />
             </Button>
-        )
-    }
-}
+            <Button
+              onClick={(e) => handleNewFolder(e, false)}
+              size="sm"
+              variant="outline"
+            >
+              File
+              <FilePlus className="ml-2 w-5 h-5 text-blue-500" />
+            </Button>
+          </div>
+        </div>
+
+        <div className={cn("ml-6 flex flex-col", expand ? "block" : "hidden")}>
+          {showInput.visible && (
+            <div className="flex gap-2 items-center transition">
+              <span>
+                {showInput.isFolder ? (
+                  <Folder className="w-5 h-5 text-amber-600" fill="#FFBF00" />
+                ) : (
+                  <File className="w-5 h-5 text-blue-500" />
+                )}
+              </span>
+
+              <Input
+                autoFocus
+                type="text"
+                onBlur={() => setShowInput({ ...showInput, visible: false })}
+                className="w-[20vw] h-8"
+                onKeyDown={onAddFolder}
+              />
+            </div>
+          )}
+          {explorer.items.map((item) => {
+            return (
+              <FileExplorer
+                handleInsertNode={handleInsertNode}
+                explorer={item}
+                key={item.id}
+              />
+            );
+          })}
+        </div>
+      </div>
+    );
+  } else {
+    return (
+      <Button className="flex items-center cursor-pointer" variant="ghost">
+        <File className="w-5 h-5 text-blue-500 mr-2" /> {explorer.name}
+      </Button>
+    );
+  }
+};
